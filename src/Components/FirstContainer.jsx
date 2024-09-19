e813f27fa7c10743c448e3a0c0d28ef5a5ef8f97
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2024-09-19 17:37:51 +0530

    Correct the code so every refresh new trailer show on the page

--- src/Components/FirstContainer.jsx
+++ src/Components/FirstContainer.jsx
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import VideoTitle from "./VideoTitle";
 import VideoBackground from "./VideoBackground";
 import { useSelector } from "react-redux";
 
 const FirstContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+  const [randomMovie, setRandomMovie] = useState(null);
 
-  // Ensure movies is defined and has at least one element
-  if (!movies || movies.length === 0) {
+  // Ensure movies are loaded and select a random movie
+  useEffect(() => {
+    if (movies && movies.length > 0) {
+      const randomIndex = Math.floor(Math.random() * movies.length);
+      setRandomMovie(movies[randomIndex]);
+    }
+  }, [movies]);
+
+  // Ensure randomMovie is defined
+  if (!randomMovie) {
     return <div>Loading...</div>; // or any fallback UI
   }
 
-  const mainMovie = movies[0];
-
-  // Ensure mainMovie is defined before destructuring
-  const { original_title = "", overview = "", id = "" } = mainMovie || {};
+  const { original_title = "", overview = "", id = "" } = randomMovie;
 
   return (
     <div className="w-full h-full">
+      {/* Pass the title and overview to VideoTitle */}
       <VideoTitle title={original_title} overview={overview} />
+      {/* Pass the selected movie ID to VideoBackground */}
       <VideoBackground movieID={id} />
     </div>
   );
--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,26 +1,70 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { API_OPTIONS } from "../utils/constant";
+// import { addTrailervideo } from "../utils/moviesSclice";
+
+// export const useMovieTrailer = (movieID) => {
+//   const dispatch = useDispatch();
+
+//   const getMovieVideos = async () => {
+//     const data = await fetch(
+//       `https://api.themoviedb.org/3/movie/${movieID}/videos`,
+//       API_OPTIONS
+//     );
+//     const json = await data.json();
+
+//     const filterData = json.results.filter((video) => video.type === "Trailer");
+
+//     const trailer = filterData.length ? filterData[0] : json.results[0];
+
+//     dispatch(addTrailervideo(trailer));
+//   };
+
+//   useEffect(() => {
+//     getMovieVideos();
+//   }, []);
+// };
+
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { API_OPTIONS } from "../utils/constant";
 import { addTrailervideo } from "../utils/moviesSclice";
 
-export const useMovieTrailer = (movieID) => {
+export const useMovieTrailer = (movieID, setError) => {
   const dispatch = useDispatch();
 
   const getMovieVideos = async () => {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieID}/videos`,
-      API_OPTIONS
-    );
-    const json = await data.json();
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieID}/videos`,
+        API_OPTIONS
+      );
 
-    const filterData = json.results.filter((video) => video.type === "Trailer");
+      if (!response.ok) {
+        throw new Error("Failed to fetch the trailer");
+      }
 
-    const trailer = filterData.length ? filterData[0] : json.results[0];
+      const json = await response.json();
 
-    dispatch(addTrailervideo(trailer));
+      const filterData = json.results.filter(
+        (video) => video.type === "Trailer"
+      );
+
+      const trailer = filterData.length ? filterData[0] : null;
+
+      if (trailer) {
+        dispatch(addTrailervideo(trailer));
+      } else {
+        setError("No trailer available");
+      }
+    } catch (err) {
+      setError(err.message);
+    }
   };
 
   useEffect(() => {
-    getMovieVideos();
-  }, []);
+    if (movieID) {
+      getMovieVideos();
+    }
+  }, [movieID]); // Re-fetch when movieID changes
 };
